647f86aad46ce58a2dd55915ce31c6c69a562c91
Elven Lin <elvenlin1998@gmail.com>
2020-01-02 11:20:46 +0800

    feat(): Fake Queue-related API Completed.

--- router/router.go
+++ router/router.go
@@ -10,4 +10,10 @@ import (
 func SetUpRouter(engine *gin.Engine) {
 	// System Info Related Route.
 	engine.GET("system/hardwareSpec", handler.GetSystemInfo)
+
+	// Queue Statics
+	engine.GET("queue/statistic", handler.GetQueueStatics)
+	engine.GET("queue/interactive", handler.GetInteractiveInfo)
+	engine.GET("queue/train", handler.GetTrainInfo)
+	engine.GET("queue/service", handler.GetServiceInfo)
 }
