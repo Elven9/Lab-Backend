c10ba54d845a36ab1eac85d5a08665ed18327064
Elven Lin <elvenlin1998@gmail.com>
2020-01-22 16:48:05 +0800

    feat(): Complete system/hardwareSpec

--- router/handler/systemInfo.go
+++ router/handler/systemInfo.go
@@ -1,6 +1,11 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os/exec"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,69 +13,64 @@ type systemInfo struct {
 	NodeType      string `json:"node_type"`
 	NodeCount     int    `json:"node_count"`
 	CPUType       string `json:"cpu_type"`
-	CPUCapacity   int    `json:"cpu_capacity"`
-	CPUCount      int    `json:"cpu_count"`
+	CPUCapacity   string `json:"cpu_capacity"`
+	CPUCount      string `json:"cpu_count"`
 	MemorySize    string `json:"memory_size"`
 	GPUType       string `json:"gqu_type"`
-	GPUCapacity   int    `json:"gpu_capacity"`
-	GPUCount      int    `json:"gpu_count"`
+	GPUCapacity   string `json:"gpu_capacity"`
+	GPUCount      string `json:"gpu_count"`
 	GPUMemorySize string `json:"gpu_memory_size"`
 }
 
 // GetSystemInfo ,Handler Func for System Info.
 func GetSystemInfo(ctx *gin.Context) {
-	// TODO: Real System Infomation Source
-	// Currently implemented with static info, hardcoded.
-	response := [4]systemInfo{
-		systemInfo{
-			NodeType:      "Test Node Type",
-			NodeCount:     5,
-			CPUType:       "i7-9700【8核/8緒】3.0GHz(↑4.7GHz)/12M/UHD630/65W",
-			CPUCapacity:   12,
-			CPUCount:      4,
-			MemorySize:    "32 GB",
-			GPUType:       "NVIDIA® Tesla® P100",
-			GPUCapacity:   12,
-			GPUCount:      2,
-			GPUMemorySize: "64 GB",
-		},
-		systemInfo{
-			NodeType:      "Test Node Type",
-			NodeCount:     5,
-			CPUType:       "i7-9700【8核/8緒】3.0GHz(↑4.7GHz)/12M/UHD630/65W",
-			CPUCapacity:   12,
-			CPUCount:      4,
-			MemorySize:    "32 GB",
-			GPUType:       "NVIDIA® Tesla® P100",
-			GPUCapacity:   12,
-			GPUCount:      2,
-			GPUMemorySize: "64 GB",
-		},
-		systemInfo{
-			NodeType:      "Test Node Type",
-			NodeCount:     5,
-			CPUType:       "i7-9700【8核/8緒】3.0GHz(↑4.7GHz)/12M/UHD630/65W",
-			CPUCapacity:   12,
-			CPUCount:      4,
-			MemorySize:    "32 GB",
-			GPUType:       "NVIDIA® Tesla® P100",
-			GPUCapacity:   12,
-			GPUCount:      2,
-			GPUMemorySize: "64 GB",
-		},
-		systemInfo{
-			NodeType:      "Test Node Type",
-			NodeCount:     5,
-			CPUType:       "i7-9700【8核/8緒】3.0GHz(↑4.7GHz)/12M/UHD630/65W",
-			CPUCapacity:   12,
-			CPUCount:      4,
-			MemorySize:    "32 GB",
-			GPUType:       "NVIDIA® Tesla® P100",
-			GPUCapacity:   12,
-			GPUCount:      2,
-			GPUMemorySize: "64 GB",
-		},
+	// Get Node Information from execution of commandline
+	var outBuf bytes.Buffer
+	cmd := exec.Command("kubectl", "get", "node", "-o", "json")
+	cmd.Stdout = &outBuf
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	ctx.JSON(200, response)
+	// Start To Parse Information
+	var nodeInfos struct {
+		Items []struct {
+			Metadata struct {
+				Name string `json:"name"`
+				Uid  string `json:"uid"`
+			} `json:"metadata"`
+			Status struct {
+				Capacity struct {
+					Cpu    string `json:"cpu"`
+					Memory string `json:"memory"`
+					Pods   string `json:"pods"`
+				} `json:"capacity"`
+			} `json:"status"`
+		} `json:"items"`
+	}
+
+	// Extract to Json
+	json.Unmarshal(outBuf.Bytes(), &nodeInfos)
+
+	// Construct final payload
+	// [TODO]: Add Real Hardware Resource
+
+	var result []systemInfo
+	for _, item := range nodeInfos.Items {
+		result = append(result, systemInfo{
+			NodeType:      item.Metadata.Name,
+			NodeCount:     1,
+			CPUType:       "TODO",
+			CPUCapacity:   item.Status.Capacity.Pods,
+			CPUCount:      item.Status.Capacity.Cpu,
+			MemorySize:    item.Status.Capacity.Memory,
+			GPUType:       "TODO",
+			GPUCapacity:   "TODO",
+			GPUCount:      "TODO",
+			GPUMemorySize: "TODO",
+		})
+	}
+
+	ctx.JSON(200, result)
 }
