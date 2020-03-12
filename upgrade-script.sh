d12140c32ec37ae19d916dee1c95dd20ad946e9d
Elven Lin <elvenlin1998@gmail.com>
2020-03-12 22:18:02 +0800

    feat(): Add Yes to Upgrade Script

--- upgrade-script.sh
+++ upgrade-script.sh
@@ -4,10 +4,10 @@ git pull
 
 # Build Image
 docker image rm elven9/lab-backend:latest
-docker image prune
+yes | docker image prune
 
 docker build -t elven9/lab-backend:latest .
-docker image prune
+yes | docker image prune
 
 # Run Container
 docker run -d --name lab-backend -p 9000:8080 --rm elven9/lab-backend:latest
