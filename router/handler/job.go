f48fe3938743f20854cc3a4fda06f973d7e39b23
Elven Lin <elvenlin1998@gmail.com>
2020-03-13 11:05:59 +0800

    feat(): Change Implementation on job.go

--- router/handler/job.go
+++ router/handler/job.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"os/exec"
+	"sort"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -21,17 +22,55 @@ type jobInfo struct {
 	State          int    `json:"state"`
 }
 
-func isStateSucceed(succeeded int) int {
-	if succeeded == 1 {
-		return 0
-	}
-	return 1
+type singleJobCondition struct {
+	LastUpdateTime string `json:"lastUpdateTime"`
+	Reason         string `json:"reason"`
+	Type           string `json:"type"`
 }
 
+// Sorting Functionality
+type jobConditions struct {
+	Conditions []singleJobCondition
+}
+
+// Interface Implementation
+func (jc *jobConditions) Populate(list []singleJobCondition) {
+	for _, item := range list {
+		jc.Conditions = append(jc.Conditions, item)
+	}
+}
+
+func (jc *jobConditions) Len() int { return len(jc.Conditions) }
+func (jc *jobConditions) Swap(i, j int) {
+	jc.Conditions[i], jc.Conditions[j] = jc.Conditions[j], jc.Conditions[i]
+}
+func (jc *jobConditions) Less(i, j int) bool {
+	iTime, _ := time.Parse(time.RFC3339, jc.Conditions[i].LastUpdateTime)
+	jTime, _ := time.Parse(time.RFC3339, jc.Conditions[j].LastUpdateTime)
+
+	return !iTime.Before(jTime)
+}
+
+func (jc *jobConditions) GetState() int {
+	// State 0: Success
+	// State 1: Running
+	// State 2: Failed
+	// State 3: Waiting
+
+	switch jc.Conditions[0].Reason {
+	case "TFJobSucceeded":
+		return 0
+	case "TFJobRunning":
+		return 1
+	}
+	return 2
+}
+
+// GetJobs ,取得 TF JOBS 的資料
 func GetJobs(ctx *gin.Context) {
 	// Get Node Information from execution of commandline
 	var outBuf bytes.Buffer
-	cmd := exec.Command("kubectl", "get", "job", "-o", "json")
+	cmd := exec.Command("kubectl", "get", "tfjob", "-o", "json")
 	cmd.Stdout = &outBuf
 	err := cmd.Run()
 	if err != nil {
@@ -49,7 +88,7 @@ func GetJobs(ctx *gin.Context) {
 			Status struct {
 				CompletionTime string `json:"completionTime"`
 				StartTime      string `json:"startTime"`
-				Succeeded      int    `json:"succeeded"`
+				Conditions     []singleJobCondition
 			} `json:"status"`
 		} `json:"items"`
 	}
@@ -63,15 +102,21 @@ func GetJobs(ctx *gin.Context) {
 		startTimeP, _ := time.Parse(time.RFC3339, item.Status.StartTime)
 		endTimeP, _ := time.Parse(time.RFC3339, item.Status.CompletionTime)
 
+		// Create Conditions Object
+		var condition jobConditions
+
+		condition.Populate(item.Status.Conditions)
+		sort.Sort(&condition)
+
 		result = append(result, jobInfo{
 			JobID:          item.Metadata.UID,
 			JobType:        "TODO",
-			SubmissionTime: "TODO",
+			SubmissionTime: item.Metadata.CreationTimestamp,
 			StartTime:      item.Status.StartTime,
 			EndTime:        item.Status.CompletionTime,
 			ExeTime:        endTimeP.Sub(startTimeP).String(),
 			WaitTime:       "TODO",
-			State:          isStateSucceed(item.Status.Succeeded),
+			State:          condition.GetState(),
 		})
 	}
 
