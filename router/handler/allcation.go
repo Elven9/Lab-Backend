358395c9701e0f9eb79e051ad3a5b7ee7e401c85
Elven Lin <elvenlin1998@gmail.com>
2020-03-14 21:13:41 +0800

    feat(): First Version Allocation

--- router/handler/allcation.go
+++ router/handler/allcation.go
@@ -9,6 +9,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type target struct {
+	Type int    `json:"type"`
+	ID   string `json:"id"`
+}
+
+type podData struct {
+	NodeID string `json:"nodeId"`
+	PodID  string `json:"podId"`
+}
+
+type userPayload struct {
+	Targets []target `json:"identifier"`
+}
+
+type responsePayload struct {
+	Target target    `json:"identifier"`
+	Data   []podData `json:"data"`
+}
+
 // GetAllocation ,就是 Allocation 的 Handler
 func GetAllocation(ctx *gin.Context) {
 	// Get Node Information from execution of commandline
@@ -44,5 +63,42 @@ func GetAllocation(ctx *gin.Context) {
 
 	json.Unmarshal(outBuf.Bytes(), &pods)
 
-	ctx.JSON(200, pods)
+	// Parse Reauest Body
+	var requestBody userPayload
+	ctx.BindJSON(&requestBody)
+
+	// Generate Response Payload
+	var res []responsePayload
+
+	for _, target := range requestBody.Targets {
+		// Construct Data Payload
+		var data []podData
+
+		for _, pod := range pods.Items {
+			if target.Type == 0 {
+				// Extract Node Data
+				if target.ID == pod.Spec.NodeName {
+					data = append(data, podData{
+						NodeID: pod.Spec.NodeName,
+						PodID:  pod.Metadata.Name,
+					})
+				}
+			} else if target.Type == 1 {
+				// Extract Specific Job
+				if target.ID == pod.Metadata.Labels.JobName {
+					data = append(data, podData{
+						NodeID: pod.Spec.NodeName,
+						PodID:  pod.Metadata.Name,
+					})
+				}
+			}
+		}
+
+		res = append(res, responsePayload{
+			Target: target,
+			Data:   data,
+		})
+	}
+
+	ctx.JSON(200, res)
 }
