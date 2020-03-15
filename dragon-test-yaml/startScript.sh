41ea44ab23880a155c1cabf26d2e9bebad2ccffb
Elven Lin <elvenlin1998@gmail.com>
2020-03-15 10:52:37 +0800

    feat(): Add Test Starter and Stoper Script

---
+++ dragon-test-yaml/startScript.sh
@@ -0,0 +1,7 @@
+# /usr/bin/zsh
+kubectl apply -f ./dragon-test-yaml/Job/job-0.yaml
+kubectl apply -f ./dragon-test-yaml/Job/job-1.yaml
+kubectl apply -f ./dragon-test-yaml/Job/job-2.yaml
+kubectl apply -f ./dragon-test-yaml/Job/job-3.yaml
+kubectl apply -f ./dragon-test-yaml/Job/job-4.yaml
+kubectl apply -f ./dragon-test-yaml/Job/job-5.yaml
