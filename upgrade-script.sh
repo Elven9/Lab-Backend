93fd4c0a1396d174cef41e438eba2722c355673f
Elven Lin <elvenlin1998@gmail.com>
2020-03-19 09:34:16 +0800

    fix: fix uncorrect sequence of commands that caused uncorrect shutdown of container.

--- upgrade-script.sh
+++ upgrade-script.sh
@@ -2,15 +2,15 @@
 # Update Code
 git pull
 
+# Stop Container
+docker container stop lab-backend
+
 # Build Image
 docker image rm elven9/lab-backend:latest
 yes | docker image prune
 
 docker build -t elven9/lab-backend:latest .
 yes | docker image prune
-
-# Stop Container
-docker container stop lab-backend
 
 # Run Container
 docker run -d --name lab-backend -p 9000:8080 --rm elven9/lab-backend:latest
