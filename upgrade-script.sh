ddce607e4e8982a49df8cfb90a763ed2c2622a25
Elven Lin <elvenlin1998@gmail.com>
2020-04-06 21:11:21 +0800

    chore: update upgrade script and remove k8s-config

--- upgrade-script.sh
+++ upgrade-script.sh
@@ -4,6 +4,7 @@ git pull
 
 # Stop Container
 docker container stop lab-backend
+docker container rm lab-backend
 
 # Build Image
 docker image rm elven9/lab-backend:latest
@@ -13,4 +14,4 @@ docker build -t elven9/lab-backend:latest .
 yes | docker image prune
 
 # Run Container
-docker run -d --name lab-backend -p 9000:8080 --rm elven9/lab-backend:latest
+docker run -d --name lab-backend -p 9000:8080 elven9/lab-backend:latest
