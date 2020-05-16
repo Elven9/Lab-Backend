6911379125587297eb1eae8f0ba76ceb24b06f1e
Elven Lin <elvenlin1998@gmail.com>
2020-05-16 09:40:50 +0800

    feat: Update Docker Compose file and add setup script

--- setup-server.sh
+++ setup-server.sh
@@ -1,5 +1,11 @@
 #/bin/zsh
 
+# Pull The Latest Image
+docker pull elven9/lab-backend:latest
+docker pull elven9/lab-monitor:latest
+docker pull grafana/grafana:latest
+docker pull nginx:latest
+
 # Set Up Environment Variable for Docker Compose to Run
 export DOCKER_COMPOSE_PWD=$(pwd)
 export FRONT_END_PORT=3000
